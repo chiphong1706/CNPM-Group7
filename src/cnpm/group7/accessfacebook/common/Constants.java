1d38f4f53fa7ee3e8039668e2337be7f24f85149
unknown <nhoktuyenpro@gmail.com>
2019-04-04 17:34:27 +0700

    add API for login fb

---
+++ src/cnpm/group7/accessfacebook/common/Constants.java
@@ -0,0 +1,8 @@
+package cnpm.group7.accessfacebook.common;
+
+public class Constants {
+	public static String FACEBOOK_APP_ID = "";
+	public static String FACEBOOK_APP_SECRET = "";
+	public static String FACEBOOK_REDIRECT_URL = "";
+	public static String FACEBOOK_LINK_GET_TOKEN = "";
+}
